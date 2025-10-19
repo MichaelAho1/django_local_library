7f8e4f1aa811aeafdd6c767c1831b21ac836f349
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-10-19 12:07:26 -0400

    .

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -51,3 +51,24 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+def index(request):
+    # …
+
+    num_authors = Author.objects.count()  # The 'all()' is implied by default.
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable.
+    return render(request, 'index.html', context=context)
